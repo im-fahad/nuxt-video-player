ef04618cce81e8efa167361c400e50751c2420a8
im-fahad <abdullahalfahad.bd@gmail.com>
2020-11-20 16:28:35 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import Player from "@/components/Player";
-
-const NuxtVideoPlayer ={
-    install(Vue){
-        Vue.component('nuxt-video-player', Player)
-    }
-};
-
-if (typeof window !== 'undefined' && window.Vue){
-    window.Vue.use(NuxtVideoPlayer)
-}
-
-export default NuxtVideoPlayer
+// import NuxtVideoPlayer from "@/components/Player";
+//
+// // const NuxtVideoPlayer ={
+// //     install(Vue){
+// //         Vue.component('nuxt-video-player', Player)
+// //     }
+// // };
+// //
+// // if (typeof window !== 'undefined' && window.Vue){
+// //     window.Vue.use(NuxtVideoPlayer)
+// // }
+//
+// export default NuxtVideoPlayer
